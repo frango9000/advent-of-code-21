c8f59c93ec4f69d53e0f35085200b457f32d1cb9
Francisco Sanchez <frango9000@gmail.com>
2022-11-08 22:44:29 +0100

    Day 4 - 1

--- packages/advent-of-code-21/src/lib/advent-of-code-21.spec.ts
+++ packages/advent-of-code-21/src/lib/advent-of-code-21.spec.ts
@@ -212,4 +212,77 @@ describe('advent of code 21', () => {
         });
     });
   });
+
+  describe('Day 4', () => {
+    class BingoCard {
+      card: number[][] = [];
+      checks: boolean[][] = [
+        [false, false, false, false, false],
+        [false, false, false, false, false],
+        [false, false, false, false, false],
+        [false, false, false, false, false],
+        [false, false, false, false, false],
+      ];
+
+      constructor(rawCard: string) {
+        this.card = rawCard.split('\n').map((item) =>
+          item
+            .trim()
+            .split(/[ ]{1,}/)
+            .map((item) => parseInt(item))
+        );
+      }
+
+      markNumberAndCheckLineWinner(number: number): boolean {
+        for (let i = 0; i < this.card.length; i++) {
+          for (let j = 0; j < this.card[i].length; j++) {
+            if (this.card[i][j] === number) {
+              this.checks[i][j] = true;
+              if (this.checks[i].every((item) => item)) return true;
+              if (this.checks.map((item) => item[j]).every((item) => item)) return true;
+              return false;
+            }
+          }
+        }
+        return false;
+      }
+
+      getFinalScore(finalNumber: number): number {
+        return (
+          this.card
+            .reduce((acc, curr, i) => {
+              return acc.concat(curr.filter((item, j) => !this.checks[i][j]));
+            }, [])
+            .reduce((acc, curr) => acc + curr, 0) * finalNumber
+        );
+      }
+    }
+
+    it('should get bingo winner', (done) => {
+      axios
+        .get('/4/input')
+        .pipe(
+          map(({ data }) => {
+            const input: string[] = data.split('\n\n');
+            const bingoSequence = input[0].split(',').map((item) => parseInt(item));
+            const bingoCards = input.slice(1).map((item) => new BingoCard(item));
+
+            for (const number of bingoSequence) {
+              for (const card of bingoCards) {
+                if (card.markNumberAndCheckLineWinner(number)) {
+                  return card.getFinalScore(number);
+                }
+              }
+            }
+            throw new Error('No bingo winner');
+          })
+        )
+        .subscribe((finalScore) => {
+          console.log('Final Score: ' + finalScore);
+
+          expect(finalScore).toBe(16716);
+          done();
+        });
+    });
+  });
 });
