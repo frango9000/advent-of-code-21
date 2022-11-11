4a2836ed01f12ef0473c2aba647d5f1971bf7741
Francisco Sanchez <frango9000@gmail.com>
2022-11-11 17:27:12 +0100

    Day 7 - 2

--- packages/advent-of-code-21/src/lib/advent-of-code-21-day-7.spec.ts
+++ packages/advent-of-code-21/src/lib/advent-of-code-21-day-7.spec.ts
@@ -2,11 +2,11 @@ import { firstValueFrom } from 'rxjs';
 import { day7_1, day7_2 } from './advent-of-code-21-day-7';
 
 describe('Day 7', () => {
-  it('should get number of lantern fish in 80 days', async () => {
+  it('should get optimal crab subs angle linear cost', async () => {
     return expect(firstValueFrom(day7_1())).resolves.toBe(336131);
   });
 
-  it.skip('should get number of lantern fish in 257 days', async () => {
-    return expect(firstValueFrom(day7_2())).resolves.toBeTruthy();
+  it('should get optimal crab subs angle scaling cost', async () => {
+    return expect(firstValueFrom(day7_2())).resolves.toBe(92676646);
   });
 });
--- packages/advent-of-code-21/src/lib/advent-of-code-21-day-7.ts
+++ packages/advent-of-code-21/src/lib/advent-of-code-21-day-7.ts
@@ -29,6 +29,60 @@ export function day7_1(): Observable<number> {
   );
 }
 
-export function day7_2(): Observable<unknown> {
-  return axios.get('/7/input').pipe();
+export function day7_2(): Observable<number> {
+  return axios.get('/7/input').pipe(
+    // return of({ data: '16,1,2,0,4,2,7,1,2,14' }).pipe(
+    map(({ data }) => {
+      const crabSubs = data
+        .trim()
+        .split(',')
+        .map((item) => Number.parseInt(item));
+
+      const { min, max } = crabSubs.reduce(
+        (acc, curr) => ({
+          min: Math.min(acc.min, curr),
+          max: Math.max(acc.max, curr),
+        }),
+        {
+          min: crabSubs[0],
+          max: crabSubs[0],
+        }
+      );
+
+      const deltas: number[] = [];
+      for (let direction = min; direction <= max; direction++) {
+        deltas.push(
+          crabSubs.reduce((acc, curr) => {
+            return acc + getIterativeDelta(Math.abs(curr - direction));
+          }, 0)
+        );
+      }
+      return deltas.reduce((acc: number, curr: number) => Math.min(acc, curr), deltas[0]);
+    })
+  );
+}
+
+export function getRecursiveDelta(distance: number): number {
+  if (distance === 0) {
+    return 0;
+  }
+  if (distance === 1) {
+    return 1;
+  }
+  return getRecursiveDelta(distance - 1) + distance;
+}
+
+export function getIterativeDelta(distance: number): number {
+  if (distance === 0) {
+    return 0;
+  }
+  if (distance === 1) {
+    return 1;
+  }
+
+  let delta = 0;
+  for (let i = 1; i <= distance; i++) {
+    delta += i;
+  }
+  return delta;
 }
