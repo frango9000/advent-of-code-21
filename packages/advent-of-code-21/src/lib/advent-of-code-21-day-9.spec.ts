5aecf2c93d4bb264bb002af592f7b9d04a282d5d
Francisco Sanchez <frango9000@gmail.com>
2022-11-12 11:35:49 +0100

    Day 9 - 2

--- packages/advent-of-code-21/src/lib/advent-of-code-21-day-9.spec.ts
+++ packages/advent-of-code-21/src/lib/advent-of-code-21-day-9.spec.ts
@@ -1,5 +1,5 @@
 import { firstValueFrom, map } from 'rxjs';
-import { day9_1, day9_2, isLinearLowpoint } from './advent-of-code-21-day-9';
+import { day9_1, day9_2 } from './advent-of-code-21-day-9';
 import { api, mapData } from './api';
 
 describe('Day 9: Smoke Basin', () => {
@@ -8,27 +8,8 @@ describe('Day 9: Smoke Basin', () => {
     return expect(firstValueFrom(api.get('/9/input').pipe(mapData, map(day9_1)))).resolves.toBe(570);
   });
 
-  it.skip('should do things', async () => {
-    return expect(firstValueFrom(day9_2())).resolves.toBe(1024649);
-  });
-
-  it('should return true if on low point', () => {
-    expect(isLinearLowpoint(4, 3, 4)).toBe(true);
-    expect(isLinearLowpoint(4, 4, 4)).toBe(false);
-    expect(isLinearLowpoint(4, 5, 4)).toBe(false);
-    expect(isLinearLowpoint(undefined, 5, 6)).toBe(true);
-    expect(isLinearLowpoint(4, 5, 6)).toBe(false);
-    expect(isLinearLowpoint(4, 5, undefined)).toBe(false);
-    expect(isLinearLowpoint(undefined, 5, 4)).toBe(false);
-    expect(isLinearLowpoint(6, 5, 4)).toBe(false);
-    expect(isLinearLowpoint(6, 5, undefined)).toBe(true);
-
-    expect(isLinearLowpoint(0, 5, 4)).toBe(false);
-    expect(isLinearLowpoint(6, 5, 4)).toBe(false);
-    expect(isLinearLowpoint(6, 5, 0)).toBe(false);
-    expect(isLinearLowpoint(6, 0, 4)).toBe(true);
-
-    expect(isLinearLowpoint(0, 0, 0)).toBe(false);
-    expect(isLinearLowpoint(6, 0, 0)).toBe(false);
+  it('should get the product of the size of the 3 largest basins', async () => {
+    expect(day9_2()).toBe(1134);
+    return expect(firstValueFrom(api.get('/9/input').pipe(mapData, map(day9_2)))).resolves.toBe(899392);
   });
 });
--- packages/advent-of-code-21/src/lib/advent-of-code-21-day-9.ts
+++ packages/advent-of-code-21/src/lib/advent-of-code-21-day-9.ts
@@ -1,6 +1,3 @@
-import { map, Observable } from 'rxjs';
-import { api } from './api';
-
 export function isLinearLowpoint(prev: number, current: number, next: number): boolean {
   return (prev === undefined || prev > current) && (next === undefined || next > current);
 }
@@ -14,6 +11,7 @@ export function day9_1(data = '2199943210\n3987894921\n9856789892\n8767896789\n9
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (
+        matrix[i][j] !== 9 &&
         isLinearLowpoint(matrix[i][j - 1], matrix[i][j], matrix[i][j + 1]) &&
         isLinearLowpoint(matrix[i - 1] && matrix[i - 1][j], matrix[i][j], matrix[i + 1] && matrix[i + 1][j])
       ) {
@@ -25,10 +23,40 @@ export function day9_1(data = '2199943210\n3987894921\n9856789892\n8767896789\n9
   return lowpoints.length + lowpoints.reduce((a, b) => a + b, 0);
 }
 
-export function day9_2(): Observable<number> {
-  return api.get('/9/input').pipe(
-    map(({ data }) => {
-      return data.trim().split('\n').length;
-    })
+export function day9_2(data = '2199943210\n3987894921\n9856789892\n8767896789\n9899965678\n'): number {
+  const matrix: number[][] = data
+    .trim()
+    .split('\n')
+    .map((line) => line.split('').map((char) => parseInt(char)));
+  const clusterSizes: number[] = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (
+        matrix[i][j] !== 9 &&
+        isLinearLowpoint(matrix[i][j - 1], matrix[i][j], matrix[i][j + 1]) &&
+        isLinearLowpoint(matrix[i - 1] && matrix[i - 1][j], matrix[i][j], matrix[i + 1] && matrix[i + 1][j])
+      ) {
+        clusterSizes.push(getBasinSize(matrix, i, j));
+      }
+    }
+  }
+
+  return clusterSizes
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a * b, 1);
+}
+
+function getBasinSize(matrix: number[][], i: number, j: number): number {
+  if (!matrix[i] || matrix[i][j] === undefined || matrix[i][j] === 9) {
+    return 0;
+  }
+  matrix[i][j] = 9;
+  return (
+    1 +
+    getBasinSize(matrix, i - 1, j) +
+    getBasinSize(matrix, i + 1, j) +
+    getBasinSize(matrix, i, j - 1) +
+    getBasinSize(matrix, i, j + 1)
   );
 }
